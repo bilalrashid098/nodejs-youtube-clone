06f0455bbd5967cc731f457b3bb1e564c6358122
bilalrashidweuno <142979450+bilalrashidweuno@users.noreply.github.com>
2024-09-08 15:19:45 +0300

    config changes for deployment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import { connectDB } from "./db/index.js";
-import { app } from "./app.js";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }) // Allow requests from the specified origin
+);
+
+app.use(express.json({ limit: "16kb" })); // limit the size of the request body
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); // url encode to read params from url
+app.use(express.static("public")); // to serve static files
+app.use(cookieParser()); // to parse cookies
+
+// routes import
+import dashboardRouter from "./routes/dashboard.routes.js";
+import userRouter from "./routes/user.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+
+// routes declaration
+app.use("/api/v1/dashboard", dashboardRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/video", videoRouter);
+app.use("/api/v1/subscription", subscriptionRouter);
+app.use("/api/v1/tweet", tweetRouter);
+app.use("/api/v1/comment", commentRouter);
+app.use("/api/v1/like", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
 
 dotenv.config({
   path: "./env",
@@ -15,3 +51,5 @@ connectDB()
   .catch((err) => {
     console.log(err);
   });
+
+export { app };
